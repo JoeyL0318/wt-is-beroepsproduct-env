a3b8b5fae27d54cbd6d6cf7715f7ed96c4f5ed00
JoeyL0318 <joey.lam2809@gmail.com>
2025-01-06 09:07:20 +0100

    Update 01-componist-new.php

--- applicatie/01-componist-new.php
+++ applicatie/01-componist-new.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'db_connectie.php';
-
+if (isset($_POST['opslaan'])); {
 // 4 kolommen, dus ook 4 variabelen
 $componistId    = $_POST['componistId'];
 $naam           = $_POST['naam'];
@@ -32,10 +32,11 @@ $succes = $query->execute($data_array);
 
 if ($succes) {
     $melding = 'Gegevens zijn opgeslagen in de database.';
+    }
 }
-else {
-    $melding = 'Er ging iets fout bij het opslaan.';
-}  
+    else {
+    $melding = '';
+    }
 ?>
 <!DOCTYPE html>
 <html lang="nl">
@@ -48,6 +49,24 @@ else {
     <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
+    <div>
+    <form action="01-componist-new.php" method="post">
+        <label for="componistId">componistId</label>
+        <input type="text" id="componistId" name="componistId"><br>
+
+        <label for="naam">naam</label>
+        <input type="text" id="naam" name="naam"><br>
+
+        <label for="geboortedatum">geboortedatum</label>
+        <input type="date" id="geboortedatum" name="geboortedatum"><br>
+
+        <label for="schoolId">schoolId</label>
+        <input type="text" id="schoolId" name="schoolId"><br>
+
+        <input type="reset" id="reset" name="reset" value="wissen">
+        <input type="submit" id="opslaan" name="opslaan" value="opslaan">    
+    </form>
+    </div>
     <?= $melding ?>
 </body>
 </html>
